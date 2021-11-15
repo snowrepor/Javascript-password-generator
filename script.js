b6c7edf325a51cf2813ac1b957712037c7e47169
Benji <snowrepor@gmail.com>
2021-11-14 21:11:56 -0700

    if else added

--- script.js
+++ script.js
@@ -25,14 +25,85 @@ alpha2 = alpha.map(toUpper);
 
 var get = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+get.addEventListener("click", function () {
+  ps = generatePassword();
+  document.getElementById("password").placeholder = ps;
+});
 
-  passwordText.value = password;
+// Start function to generate password
+function generatePassword() {
+  // Asks for user input
+  enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+  // First if statement for user validation 
+  if (!enter) {
+      alert("This needs a value");
+  } else if (enter < 8 || enter > 128) {
+      // Validates user input
+      // Start user input prompts
+      enter = parseInt(prompt("You must choose between 8 and 128"));
 
-}
+  } else {
+      // Continues once user input is validated
+      confirmNumber = confirm("Will this contain numbers?");
+      confirmCharacter = confirm("Will this contain special characters?");
+      confirmUppercase = confirm("Will this contain Uppercase letters?");
+      confirmLowercase = confirm("Will this contain Lowercase letters?");
+  };
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Else if for 4 negative options
+  if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+      choices = alert("You must choose a criteria!");
+
+  }
+  // First if statement that uses user input prompts to determine choices
+  // Else if for 4 positive options
+  else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+
+      choices = character.concat(number, alpha, alpha2);
+  }
+  // Else if for 3 positive options
+  else if (confirmCharacter && confirmNumber && confirmUppercase) {
+      choices = character.concat(number, alpha2);
+  }
+  else if (confirmCharacter && confirmNumber && confirmLowercase) {
+      choices = character.concat(number, alpha);
+  }
+  else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+      choices = character.concat(alpha, alpha2);
+  }
+  else if (confirmNumber && confirmLowercase && confirmUppercase) {
+      choices = number.concat(alpha, alpha2);
+  }
+  // Else if for 2 positive options 
+  else if (confirmCharacter && confirmNumber) {
+      choices = character.concat(number);
+
+  } else if (confirmCharacter && confirmLowercase) {
+      choices = character.concat(alpha);
+
+  } else if (confirmCharacter && confirmUppercase) {
+      choices = character.concat(alpha2);
+  }
+  else if (confirmLowercase && confirmNumber) {
+      choices = alpha.concat(number);
+
+  } else if (confirmLowercase && confirmUppercase) {
+      choices = alpha.concat(alpha2);
+
+  } else if (confirmNumber && confirmUppercase) {
+      choices = number.concat(alpha2);
+  }
+  // Else if for 1 positive option
+  else if (confirmCharacter) {
+      choices = character;
+  }
+  else if (confirmNumber) {
+      choices = number;
+  }
+  else if (confirmLowercase) {
+      choices = alpha;
+  }
+  // Created space variable to fill uppercase conversion
+  else if (confirmUppercase) {
+      choices = space.concat(alpha2);
+  };
